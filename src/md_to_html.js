37dc6b512029c83b6a1bf1753e3b42e68fa08824
nakajmg <nakajima.jmg@gmail.com>
2015-03-23 14:24:26 +0900

    change highlighter

--- src/md_to_html.js
+++ src/md_to_html.js
@@ -1,10 +1,24 @@
 import marked from "marked";
 import hljs from "highlight.js";
 import {injectHTMLBlock} from "./inject_html_block";
+import Highlights from "highlights";
 
 marked.setOptions({
-  highlight: function (code) {
-    return hljs.highlightAuto(code).value;
+  highlight: function (code, type) {
+    var scope = {
+      js: "source.js",
+      javascript: "source.js",
+      html: "text.html.gohtml",
+      block: "text.html.gohtml"
+    };
+    var scopeName = scope[type] || "text.html.gohtml";
+    
+    var highlighter = new Highlights();
+    var html = highlighter.highlightSync({
+      fileContents: code,
+      scopeName
+    });
+    return html;
   }
 });
 
