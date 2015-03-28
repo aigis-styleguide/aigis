55041ec9c0766489aee25849e43ad923d157824a
nakajmg <nakajima.jmg@gmail.com>
2015-03-28 15:14:48 +0900

    add Config Model

---
+++ src3/Config.js
@@ -0,0 +1,40 @@
+import objectAssign from "object-assign";
+import {EventEmitter2 as EventEmitter} from "eventemitter2";
+import _ from "lodash";
+import del from "del";
+import fs from "fs";
+import vfs from "vinyl-fs";
+import CSON from "cson";
+import path from "path";
+import defaultConfig from "./defaultConfig";
+
+export default class Config extends EventEmitter {
+  constructor(opts) {
+    super(opts);
+    this._eventify();
+    this._loadConfigCson();
+  }
+  _eventify() {
+    this.on("end:loadconfigcson", this._onEndLoadConfigCson);
+  }
+  _loadConfigCson() {
+    var filePath = path.resolve("./config.cson");
+    var cson;
+    try {
+      cson = fs.readFileSync(filePath, "utf8");
+    }
+    catch (err) {
+      // console.log(err.message);
+    }
+    finally {
+      if (cson) {
+        cson = CSON.parse(cson);
+      }
+      this.emit("end:loadconfigcson", cson);
+    }
+  }
+  _onEndLoadConfigCson(json) {
+    objectAssign(this, defaultConfig, json);
+    console.log(this);
+  }
+}
