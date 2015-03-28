57ce7d6ca4afa49541963ec2677a115521ed2f4b
nakajmg <nakajima.jmg@gmail.com>
2015-03-28 15:14:36 +0900

    add defaultConfig

---
+++ src3/defaultConfig.js
@@ -0,0 +1,23 @@
+var defaultConfig;
+
+export default defaultConfig = {
+  name: "hoge",
+  source: ["./css"],
+  dest: "./ronde",
+  doc_assets: ["./doc_assets"],
+  lib: {},
+  md_template: "./md_template",
+  md_class: {
+    code:       [],
+    blockquote: [],
+    html:       [],
+    heading:    [],
+    hr:         [],
+    list:       [],
+    listitem:   [],
+    paragraph:  [],
+    table:      [],
+    tablerow:   [],
+    tablecell:  [],
+  },
+};
