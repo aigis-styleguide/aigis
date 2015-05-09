c2f97dcd07f9d215c733d17c686c8407f74db52d
nakajmg <nakajima.jmg@gmail.com>
2015-05-10 03:23:13 +0900

    support sass and stylus

--- src/lib/parseCSS.js
+++ src/lib/parseCSS.js
@@ -1,13 +1,38 @@
+import path from "path";
 import through from "through2";
 import objectAssign from "object-assign";
 import _ from "lodash";
 import CSS from "css";
 import CSON from "cson";
+import stylus from "stylus";
+import sass from "node-sass";
 
 export default function parseCSS () {
   var comments = [];
   return through.obj(function(file, enc, cb) {
-    var css = CSS.parse(file.contents.toString(), {source: file.path});
+    var css, content;
+    var ext  = path.extname(file.path);
+    var source = file.path;
+    
+    console.log(file.path);
+    switch(ext.toLowerCase()) {
+      case ".css":
+        content = file.contents.toString();
+        break;
+      case ".styl":
+        alt = stylus.render(file.contents.toString());
+        content = alt;
+        break;
+      case ".scss":
+      case ".sass":
+        alt = sass.renderSync({
+          data: file.contents.toString()
+        });
+        content = alt.css.toString();
+        break;
+    }
+    css = CSS.parse(content, {source});
+
     var _this = this;
     _.chain(css.stylesheet.rules)
       .filter({type: "comment"})
