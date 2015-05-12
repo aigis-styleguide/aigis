643e3118949e777b175b0463e17bc77c8713c796
nakajmg <nakajima.jmg@gmail.com>
2015-05-13 01:13:51 +0900

    htmlの正規表現を修正

--- src/lib/injector/html.js
+++ src/lib/injector/html.js
@@ -5,7 +5,7 @@ import through from "through2";
  * inject htmlcode from codeblock to markdown
  */
 export default function html({inject}) {
-  var reg_block = /`{3}(html|block|\n)+[\s\S]*?`{3}/;
+  var reg_block = /`{3}(html|block)+[\s\S]*?`{3}/;
   var reg_start = /`{3}(html|block|\n)+/;
   var reg_end = /`{3}/;
   
