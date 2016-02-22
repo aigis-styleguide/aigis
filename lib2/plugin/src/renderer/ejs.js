bcb01223794d6247dcff63a01eafbb65ba8d6d4f
nakajmg <nakajima.jmg@gmail.com>
2016-02-22 09:45:01 +0900

    update ejs_renderer

--- lib2/plugin/src/renderer/ejs.js
+++ lib2/plugin/src/renderer/ejs.js
@@ -1,43 +1,51 @@
 var ejs = require('ejs');
 var fs = require('fs-extra');
 var path = require('path');
+var _ = require('lodash');
+var Moment = require("moment");
+var system = require('../../../system');
+var reader = require('../../../reader');
 var helper = require('../../../renderer/helper');
 
-function ejs_render(modules, options) {
-  var indexPath = path.resolve(options.template);
-  var template = fs.readFileSync(indexPath, 'utf-8');
-//  var outputPath = path.join(options.dest, type, name, "index.html");
-  var root = './';
-
-  helper.init(options, root);
-
-  console.log(ejs.render(template, {
-    modules: modules,
-    config: options,
-    root: root,
-    timestamp: '2016/2/21 23:30',
-    helper: helper
-  }, {filename: indexPath}));
-}
-
-function getRoot(outputPath) {
-  var level = _.compact(outputPath
-      .replace(path.normalize(this.options.dest + "/"), "")
-      .replace("index.html", "")
-      .split("/")
-  ).length;
-  var dots = "";
-  if (level === 0) {
-    return "./";
+var EJS_Renderer = (function() {
+  function EJS_Renderer(options, modules) {
+    this.options = options;
+    this.modules = modules;
+    this.timestamp = system.timestamp.get(options);
+    this.indexTemplate = reader.ejs(options);
   }
 
-  for(var i = 0; i < level; i++) {
-    dots += "../";
-  }
+  EJS_Renderer.prototype = {
+    constructor: EJS_Renderer,
 
-  return dots;
-}
+    render: function() {
+      return this.renderPage({
+        modules: this.modules,
+        type: 'category',
+        name: 'hoge'
+      });
+    },
 
-module.exports = ejs_render;
+    renderPage: function(params) {
+      var type = params.type, name = params.name, modules = params.modules;
+      var outputPath = path.join(this.options.dest, type, name, 'index.html');
+      var root = system.getRoot(outputPath, this.options);
+      helper.init(this.options, root);
 
+      var page = this.indexTemplate({
+        modules: modules,
+        config: this.options,
+        timestamp: this.timestamp,
+        root: root,
+        helper: helper
+      });
+
+      return page;
+    }
+  };
+
+  return EJS_Renderer;
+})();
+
+module.exports = EJS_Renderer;
 
