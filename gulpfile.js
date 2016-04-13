fbc5e8540141be31f33607c041180585f620c96f
nakajmg <nakajima.jmg@gmail.com>
2016-04-13 15:02:50 +0900

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,13 @@ var process = require('child_process');
 var exec = process.exec;
 var src = {
   sass: ['src/sass/*.scss'],
-  docs_jp: ['src/template/*.ejs','src/doc/jp/**/*.md', 'src/aigis_config_jp.yml'],
-  docs_en: ['src/template/*.ejs','src/doc/en/**/*.md', 'src/aigis_config_jp.yml']
+  docs_jp: ['src/template/*.ejs','src/jp/**/*.md', 'src/aigis_config_jp.yml'],
+  docs_en: ['src/template/*.ejs','src/en/**/*.md', 'src/aigis_config_en.yml']
 };
 var dest = {
   css: './src/doc_assets/css',
-  doc_css: './docs/doc_assets/css'
+  doc_en: './docs/en/doc_assets/css',
+  doc_jp: './docs/jp/doc_assets/css'
 };
 
 gulp.task('sass', () => {
@@ -20,32 +21,36 @@ gulp.task('sass', () => {
     .pipe(plumber())
     .pipe(sass())
     .pipe(gulp.dest(dest.css))
-    .pipe(gulp.dest(dest.doc_css));
+    .pipe(gulp.dest(dest.doc_jp))
+    .pipe(gulp.dest(dest.doc_en));
 });
 
-gulp.task('aigis', (cb) => {
-  exec('npm run docs', (err, stdout, stderr) => {
+gulp.task('aigis_en', (cb) => {
+  exec('npm run docs_en', (err, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    cb();
+  });
+});
+gulp.task('aigis_jp', (cb) => {
+  exec('npm run docs_jp', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     cb();
   });
 });
 
-gulp.task('watch_jp', () => {
+gulp.task('watch', (cb) => {
   gulp.watch(src.sass, ['sass', reload]);
-  gulp.watch(src.docs_jp, ['aigis', reload]);
-});
-gulp.task('watch_en', () => {
-  gulp.watch(src.sass, ['sass', reload]);
-  gulp.watch(src.docs_en, ['aigis', reload]);
+  gulp.watch(src.docs_jp, ['aigis_jp', reload]);
+  gulp.watch(src.docs_en, ['aigis_en', reload]);
+  cb();
 });
 
-gulp.task('serve', ['watch_jp', 'watch_en'], () => {
+gulp.task('serve', ['watch'], function() {
   bs.init({
     server: {
-      baseDir: ['./'],
-      directory: false,
-      index: 'index.html'
+      directory: false
     },
     notify: false,
     host: 'localhost'
