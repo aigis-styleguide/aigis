365642eea1f7ec91cc3877158be041c12ec649b4
okmttdhr <10101114fin@gmail.com>
2016-12-10 10:43:06 +0900

    remove existing tests

--- test/reader/config.js
+++
@@ -1,9 +0,0 @@
-var assert = require("power-assert");
-var readConfigFile = require("../../lib/reader/config");
-
-describe("lib/reader/config", function() {
-  it("config.yamlが読み込めること", function() {
-    var config = readConfigFile(__dirname + "/../test_assets/config.yaml");
-    assert.equal(config.name, "test_project");
-  });
-});
